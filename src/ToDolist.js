9dfeae12782e84e07a2732aec6eabc9d97b9371c
madhu <119298947+Madhu579@users.noreply.github.com>
2024-07-26 13:18:14 +0530

    Add files via upload

---
+++ src/ToDolist.js
@@ -0,0 +1,87 @@
+import { useState,useEffect } from "react";
+
+
+export const ToDolist = () => {
+
+    function  getStored(){
+        let data=localStorage.getItem("todos")
+        let jsondata = JSON.parse(data)
+            return jsondata || []
+    }
+
+    const [todos,setTodos]=useState(getStored)
+
+    useEffect(()=>{
+        localStorage.setItem("todos",JSON.stringify(todos))
+    },[todos])
+
+    function handleSubmitTask(e){
+        e.preventDefault();
+        let task=e.target.task.value;
+        if(!task){
+            alert("Plese provide a valid task!")
+            return
+        }
+        setTodos([...todos, {task:task,completed:false}])
+        e.target.reset()
+    }
+
+    function changestatus(index){
+        let newTodoList = [...todos]
+        newTodoList[index].completed=!newTodoList[index].completed
+        setTodos(newTodoList)
+    }
+
+    function deleteTask(index){
+        let newTodoList=[...todos]
+        newTodoList.splice(index,1)
+        setTodos(newTodoList)
+    }
+
+    function updateTask(index){
+        let newTodoList = [...todos]
+        let data=newTodoList[index].task
+        let updatedData=prompt("update the task ",data)
+        if(updatedData){
+            newTodoList[index].task=updatedData
+        }
+        else{
+            alert("enter task to update")
+        }
+        setTodos(newTodoList)
+    }
+
+  return (
+    <div className="container my-5">
+        <div className="mx-auto rounded border p-4 bg-secondary" style={{maxWidth:"600px"}}>
+            <h1 className="text-white text-center mb-5 ">My ToDo List</h1>
+            <form className="d-flex" onSubmit={handleSubmitTask}>
+                <input className="form-control me-2" placeholder="New Task" name="task"/>
+                <button className="btn btn-outline-light" typr="submit">Add</button>
+            </form>
+            <div className="overflow-scroll" style={{height:"400px",scrollbarWidth:"none"}}>
+            {
+                todos.map((todo,index)=>{
+                    return(
+                        <div key={index} className="rounded mt-4 p-2 d-flex" 
+                            style={{ backgroundColor: todo.completed ? "greenyellow" : "lightgray"}}>
+                           <div className="me-auto">
+                            {todo.task}
+                           </div>
+                           <div >
+                            <i className={"me-2 "+(todo.completed?"bi bi-check-square":"bi bi-square")}
+                             style={{cursor:"pointer"}} 
+                             onClick={()=>{changestatus(index)}}></i>
+                            <i className="me-2 text-primary-emphasis bi bi-pencil-square" style={{cursor:"pointer"}} onClick={()=>{updateTask(index)}}></i>
+                            <i className="bi bi-trash text-danger" style={{cursor:"pointer"}} onClick={()=>{deleteTask(index)}}></i>
+                           </div>
+                        </div>
+                    );
+                })
+            }
+            </div>
+            
+        </div>
+    </div>
+  )
+}
